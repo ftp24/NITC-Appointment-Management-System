e68c83650d2756bf8522ad9a341f38042b89187e
Jesvin Sebastian Madona <sebastianjesvin@gmail.com>
2021-11-30 15:36:51 +0530

    Added FacultyCard,faculty day schedule, add appointment pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch, useHistory} from "react-router-dom";
+import FacultyScheduleDay from "./components/faculty/FacultyScheduleDay"
+import AddAppointment from "./components/student/AddAppointment"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<Router>
+				<Switch>
+					<Route exact path="/faculty-schedule-day"><FacultyScheduleDay/></Route>
+					<Route exact path="/student-add"><AddAppointment/></Route>
+				</Switch>
+		</Router>
   );
 }
 
